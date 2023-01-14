967956c6b7ec5de5210b316159f0e39d14bc7ed4
ben.tintin.bp@gmail.com <ben.tintin.bp@gmail.com>
2023-01-14 10:31:41 +0100

    add type hint !

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter
 from tkinter import *
-from tkinter import messagebox as mb
+from tkinter import messagebox as mb, Frame, Text
+from typing import Tuple, List, Dict
 
 
 class Case:
@@ -39,22 +40,24 @@ class Case:
 
 
 class Game:
+
     def __init__(self):
-        self.__amount_players = None
-        self.__rows = None
-        self.__columns = None
-        self.__place = False
-        self.__cases = None
-        self.__nextPlayerCanvas = None
-        self.__playerSelectCanvas = None
-        self.__rowText = None
-        self.__columnText = None
-        self.__selectionFrame = None
-        self.__isStarted = False
-        self.__players = []
-        self.__counter = 1
-        self.__gameFrame = None
-        self.__colors = {1: "blue", 2: "red", 3: "yellow", 4: "green", 5: "orange", 6: "pink", 7: "purple", 8: "cyan"}
+        self.__amount_players: int = None
+        self.__rows: int = None
+        self.__columns: int = None
+        self.__place: bool = False
+        self.__cases: List[List[Case]] = []
+        self.__next_player_canvas: Canvas = None
+        self.__player_select_canvas: Canvas = None
+        self.__rowText: Text = None
+        self.__columnText: Text = None
+        self.__selection_frame: Frame = None
+        self.__is_started: bool = False
+        self.__players: List[int] = []
+        self.__counter: int = 1
+        self.__game_frame: Frame = None
+        self.__colors: Dict[int, str] = {1: "blue", 2: "red", 3: "yellow", 4: "green", 5: "orange", 6: "pink",
+                                         7: "purple", 8: "cyan"}
 
         self.__root = tkinter.Tk()
         self.__root.title("Jeux python")
@@ -66,37 +69,37 @@ class Game:
 
     def show_selection(self):
 
-        self.__selectionFrame = tkinter.Frame(self.__root)
-        self.__selectionFrame.config(bg='white')
-        self.__selectionFrame.grid(row=0, column=1, padx=25)
+        self.__selection_frame = tkinter.Frame(self.__root)
+        self.__selection_frame.config(bg='white')
+        self.__selection_frame.grid(row=0, column=1, padx=25)
 
-        columnLabel = Label(self.__selectionFrame, text="Nombre de colonne(s) : ")
+        columnLabel = Label(self.__selection_frame, text="Nombre de colonne(s) : ")
         columnLabel.config(bg='white')
         columnLabel.grid(row=0, column=0)
 
-        self.__columnText = Text(self.__selectionFrame, height=1, width=20, bg='white')
+        self.__columnText = Text(self.__selection_frame, height=1, width=20, bg='white')
 
         self.__columnText.grid(row=0, column=1)
 
-        rowLabel = Label(self.__selectionFrame, text="Nombre de ligne(s) : ")
+        rowLabel = Label(self.__selection_frame, text="Nombre de ligne(s) : ")
 
         rowLabel.config(bg='white')
         rowLabel.grid(row=1, column=0)
 
-        self.__rowText = Text(self.__selectionFrame, height=1, width=20, bg='white')
+        self.__rowText = Text(self.__selection_frame, height=1, width=20, bg='white')
 
         self.__rowText.grid(row=1, column=1)
 
-        self.__playerSelectCanvas = tkinter.Frame(self.__selectionFrame)
+        self.__player_select_canvas = tkinter.Frame(self.__selection_frame)
 
-        self.__playerSelectCanvas.config(bg='white')
-        self.__playerSelectCanvas.grid(row=5, column=0)
+        self.__player_select_canvas.config(bg='white')
+        self.__player_select_canvas.grid(row=5, column=0)
 
         currentColumn = 0
         currentRow = 0
         for colors in self.__colors:
             color = self.__colors[colors]
-            canvas = Canvas(self.__playerSelectCanvas, width=64, height=64)
+            canvas = Canvas(self.__player_select_canvas, width=64, height=64)
 
             canvas.create_oval(8, 56, 56, 8, fill=color)
 
@@ -110,7 +113,7 @@ class Game:
                                                                                                  selectColor))
             currentColumn += 1
 
-        startButton = Button(self.__selectionFrame, height=1, width=10, text="Commencez",
+        startButton = Button(self.__selection_frame, height=1, width=10, text="Commencez",
                              command=lambda: self.canStart())
         startButton.config(bg='white')
         startButton.grid(row=2, column=0)
@@ -128,16 +131,18 @@ class Game:
         columnResult = self.__columnText.get("1.0", "end-1c")
         rowResult = self.__rowText.get("1.0", "end-1c")
         if not columnResult.isnumeric():
-            mb.showerror("Erreur", "Veuillez choisir un nombre entre 5 et 9 \ndans la case colonne !")
+            mb.showerror("Erreur", "Veuillez choisir un nombre entre 3 et 12 \ndans la case colonne !")
             return
         if not rowResult.isnumeric():
-            mb.showerror("Erreur", "Veuillez choisir un nombre entre 5 et 9 \ndans la case ligne !")
+            mb.showerror("Erreur", "Veuillez choisir un nombre entre 3 et 10 \ndans la case ligne !")
             return
-        columns = eval(columnResult)
-        rows = eval(rowResult)
+        columns = int(eval(columnResult))
+        rows = int(eval(rowResult))
 
-        if self.is_good_game_size((int(columns)), int(rows)):
+        if 3 <= rows <= 10 and 3 <= columns <= 12:
             if len(self.__players) > 1:
+                self.__rows = rows
+                self.__columns = columns
                 self.startGame()
             else:
                 mb.showerror("Erreur", "Veuillez choisir minimum 2 joueurs !")
@@ -145,8 +150,8 @@ class Game:
             mb.showerror("Erreur", "Veuillez choisir une taille de jeux conforme !")
 
     def startGame(self):
-        self.__selectionFrame.destroy()
-        self.__isStarted = True
+        self.__selection_frame.destroy()
+        self.__is_started = True
 
         self.__place = False
         self.__cases = [[Case(self.case_type(i, j), 0, 0) for j in range(self.__columns)] for i in
@@ -155,9 +160,9 @@ class Game:
         self.__counter = 1
         self.show_next_player()
 
-        self.__gameFrame = tkinter.Frame(self.__root)
+        self.__game_frame = tkinter.Frame(self.__root)
         self.show_game()
-        self.__gameFrame.grid(row=0, column=0, rowspan=5)
+        self.__game_frame.grid(row=0, column=0, rowspan=5)
 
     def has_winner(self):
         if len(self.__players) == 1:
@@ -166,7 +171,7 @@ class Game:
             return False
 
     def handle_round(self):
-        if not self.__isStarted:
+        if not self.__is_started:
             return
         if not self.has_winner():
             if self.__place:
@@ -191,20 +196,20 @@ class Game:
             return False
 
     def show_next_player(self):
-        if not self.__isStarted:
+        if not self.__is_started:
             return
-        self.__nextPlayerCanvas = Canvas(self.__root, width=64, height=64)
-        self.__nextPlayerCanvas.create_oval(8, 56, 56, 8, fill=self.__colors[self.__players[self.__counter - 1]])
+        self.__next_player_canvas = Canvas(self.__root, width=64, height=64)
+        self.__next_player_canvas.create_oval(8, 56, 56, 8, fill=self.__colors[self.__players[self.__counter - 1]])
 
-        self.__nextPlayerCanvas.grid(pady=10, column=0, row=5)
-        self.__nextPlayerCanvas.config(bg='black', borderwidth=0, highlightthickness=0)
+        self.__next_player_canvas.grid(pady=10, column=0, row=5)
+        self.__next_player_canvas.config(bg='black', borderwidth=0, highlightthickness=0)
 
     def game_finish(self):
         self.show_selection()
-        self.__gameFrame.destroy()
-        self.__nextPlayerCanvas.delete("all")
+        self.__game_frame.destroy()
+        self.__next_player_canvas.delete("all")
         mb.showinfo("Gagnant", "le joueur " + self.__colors[self.__players[0]] + " a gagner !")
-        self.__isStarted = False
+        self.__is_started = False
         self.clear_game()
 
     def can_place_pawn(self):
@@ -215,114 +220,106 @@ class Game:
         else:
             return False
 
-    def is_good_game_size(self, column, row):
-        if 3 <= row <= 10 and 3 <= column <= 12:
-            self.__rows = row
-            self.__columns = column
-            return True
-        else:
-
-            return False
-
-
     def show_game(self):
-        if not self.__isStarted:
+        if not self.__is_started:
             return
-        coordonate_1_pionts = (16, 48)
-        coordonate_2_pionts = (8, 28, 28, 48)
-        coordonate_3_pionts = (8, 24, 24, 40, 40, 56)
+        location_1: Tuple[int, int] = (16, 48)
+        location_2: Tuple[int, int, int, int] = (8, 28, 28, 48)
+        location_3: Tuple[int, int, int, int, int, int] = (8, 24, 24, 40, 40, 56)
 
         for x in range(len(self.__cases)):
             for y in range(len(self.__cases[x])):
                 case = self.__cases[x][y]
                 if case.has_changed():
                     case.set_changed(False)
-                    canvas1 = tkinter.Canvas(self.__gameFrame)
-                    canvas1.grid(row=x, column=y, padx=0, pady=0)
-                    canvas1.config(width=64, height=64, highlightbackground="red", highlightcolor="red",
-                                   highlightthickness=1, bg='#004F61')
+                    pawn_canvas = tkinter.Canvas(self.__game_frame)
+                    pawn_canvas.grid(row=x, column=y, padx=0, pady=0)
+                    pawn_canvas.config(width=64, height=64, highlightbackground="red", highlightcolor="red",
+                                       highlightthickness=1, bg='#004F61')
 
                     if case.get_pawn() == 1:
-                        canvas1.create_oval(x + coordonate_1_pionts[0], y + coordonate_1_pionts[0],
-                                            x + coordonate_1_pionts[1], y + coordonate_1_pionts[1],
-                                            fill=self.__colors[case.get_player()])
+                        pawn_canvas.create_oval(x + location_1[0], y + location_1[0],
+                                                x + location_1[1], y + location_1[1],
+                                                fill=self.__colors[case.get_player()])
                     elif case.get_pawn() == 2:
-                        canvas1.create_oval(x + coordonate_2_pionts[0], y + coordonate_2_pionts[0],
-                                            x + coordonate_2_pionts[1], y + coordonate_2_pionts[1],
-                                            fill=self.__colors[case.get_player()])
-                        canvas1.create_oval(x + coordonate_2_pionts[2], y + coordonate_2_pionts[2],
-                                            x + coordonate_2_pionts[3], y + coordonate_2_pionts[3],
-                                            fill=self.__colors[case.get_player()])
+                        pawn_canvas.create_oval(x + location_2[0], y + location_2[0],
+                                                x + location_2[1], y + location_2[1],
+                                                fill=self.__colors[case.get_player()])
+                        pawn_canvas.create_oval(x + location_2[2], y + location_2[2],
+                                                x + location_2[3], y + location_2[3],
+                                                fill=self.__colors[case.get_player()])
                     elif case.get_pawn() == 3:
-                        canvas1.create_oval(x + coordonate_3_pionts[0], y + coordonate_3_pionts[0],
-                                            x + coordonate_3_pionts[1], y + coordonate_3_pionts[1],
-                                            fill=self.__colors[case.get_player()])
-                        canvas1.create_oval(x + coordonate_3_pionts[2], y + coordonate_3_pionts[2],
-                                            x + coordonate_3_pionts[3], y + coordonate_3_pionts[3],
-                                            fill=self.__colors[case.get_player()])
-                        canvas1.create_oval(x + coordonate_3_pionts[4], y + coordonate_3_pionts[4],
-                                            x + coordonate_3_pionts[5], y + coordonate_3_pionts[5],
-                                            fill=self.__colors[case.get_player()])
-                    canvas1.bind('<Button-1>', lambda event, coord=(x, y): self.play(coord))
+                        pawn_canvas.create_oval(x + location_3[0], y + location_3[0],
+                                                x + location_3[1], y + location_3[1],
+                                                fill=self.__colors[case.get_player()])
+                        pawn_canvas.create_oval(x + location_3[2], y + location_3[2],
+                                                x + location_3[3], y + location_3[3],
+                                                fill=self.__colors[case.get_player()])
+                        pawn_canvas.create_oval(x + location_3[4], y + location_3[4],
+                                                x + location_3[5], y + location_3[5],
+                                                fill=self.__colors[case.get_player()])
+                    pawn_canvas.bind('<Button-1>', lambda event, coord=(x, y): self.play(coord))
 
     def play(self, coord):
-        coordX = coord[0]
-        coordY = coord[1]
-        case = self.__cases[coordX][coordY]
+        coord_x = coord[0]
+        coord_y = coord[1]
+        case = self.__cases[coord_x][coord_y]
         if case.get_player() == self.__players[self.__counter - 1] or case.get_player() == 0:
             case.set_changed(True)
             if case.get_pawn() == case.get_max_pawn():
-                self.blast(coordX, coordY)
+                self.blast(coord_x, coord_y)
             else:
-                self.put_pawn(coordX, coordY)
+                self.put_pawn(coord_x, coord_y)
             if self.handle_round():
                 self.show_game()
 
-    def put_pawn(self, coordX, coordY):
-        self.__cases[coordX][coordY].set_player(self.__players[self.__counter - 1])
-        self.__cases[coordX][coordY].increment_pawn()
+    def put_pawn(self, coord_x, coord_y):
+        self.__cases[coord_x][coord_y].set_player(self.__players[self.__counter - 1])
+        self.__cases[coord_x][coord_y].increment_pawn()
 
-    def case_type(self, coordX, coordY):
-        if coordX == 0 and coordY == 0:
+    def case_type(self, coord_x, coord_y):
+        if coord_x == 0 and coord_y == 0:
             return 1  # La case est un coin
-        if coordX == 0 and coordY == self.__columns - 1:
+        if coord_x == 0 and coord_y == self.__columns - 1:
             return 1  # La case est un coin
-        if coordX == self.__rows - 1 and coordY == 0:
+        if coord_x == self.__rows - 1 and coord_y == 0:
             return 1  # La case est un coin
-        if coordX == self.__rows - 1 and coordY == self.__columns - 1:
+        if coord_x == self.__rows - 1 and coord_y == self.__columns - 1:
             return 1  # La case est un coin
-        if coordX == 0:
+        if coord_x == 0:
             return 2  # La case est un côté
-        if coordX == self.__rows - 1:
+        if coord_x == self.__rows - 1:
             return 2  # La case est un côté
-        if coordY == 0:
+        if coord_y == 0:
             return 2  # La case est un côté
-        if coordY == self.__columns - 1:
+        if coord_y == self.__columns - 1:
             return 2  # La case est un côté
         else:
             return 3  # La case n'est ni un côté ni un coin
 
-    def blast(self, coordX, coordY):  # Je change l'attribut de la couleur joueur des cases   A FAIRE
-        self.__cases[coordX][coordY].set_pawn(0)
-        self.__cases[coordX][coordY].set_player(0)
-        if coordX != 0:
-            self.__cases[coordX - 1][coordY].set_player(self.__players[self.__counter - 1])
-            self.chain_reaction(coordX - 1, coordY)
-        if coordX != self.__rows - 1:
-            self.__cases[coordX + 1][coordY].set_player(self.__players[self.__counter - 1])
-            self.chain_reaction(coordX + 1, coordY)
-        if coordY != 0:
-            self.__cases[coordX][coordY - 1].set_player(self.__players[self.__counter - 1])
-            self.chain_reaction(coordX, coordY - 1)
-        if coordY != self.__columns - 1:
-            self.__cases[coordX][coordY + 1].set_player(self.__players[self.__counter - 1])
-            self.chain_reaction(coordX, coordY + 1)
+    def blast(self, coord_x, coord_y):
+        self.__cases[coord_x][coord_y].set_pawn(0)
+        self.__cases[coord_x][coord_y].set_player(0)
 
-    def chain_reaction(self, coordX, coordY):
-        if self.__cases[coordX][coordY].get_pawn() == self.__cases[coordX][coordY].get_max_pawn():
-            self.blast(coordX, coordY)
+        player = self.__players[self.__counter - 1]
+        if coord_x != 0:
+            self.__cases[coord_x - 1][coord_y].set_player(player)
+            self.chain_reaction(coord_x - 1, coord_y)
+        if coord_x != self.__rows - 1:
+            self.__cases[coord_x + 1][coord_y].set_player(player)
+            self.chain_reaction(coord_x + 1, coord_y)
+        if coord_y != 0:
+            self.__cases[coord_x][coord_y - 1].set_player(player)
+            self.chain_reaction(coord_x, coord_y - 1)
+        if coord_y != self.__columns - 1:
+            self.__cases[coord_x][coord_y + 1].set_player(player)
+            self.chain_reaction(coord_x, coord_y + 1)
+
+    def chain_reaction(self, coord_x, coord_y):
+        if self.__cases[coord_x][coord_y].get_pawn() == self.__cases[coord_x][coord_y].get_max_pawn():
+            self.blast(coord_x, coord_y)
         else:
-            self.put_pawn(coordX, coordY)
+            self.put_pawn(coord_x, coord_y)
 
     def clear_game(self):
         self.__players = []
